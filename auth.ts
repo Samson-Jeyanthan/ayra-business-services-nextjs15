9b9e80ab3fe1f5474aba078203ed8642bedf8477
Samson <samsonjeyanthan5@gmail.com>
2026-02-14 02:02:28 +0530

    SSR issue trying to fix - 9

--- auth.ts
+++ auth.ts
@@ -6,6 +6,7 @@ import { SignInSchema } from "./lib/validations";
 import { api } from "./lib/api";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       async authorize(credentials) {
