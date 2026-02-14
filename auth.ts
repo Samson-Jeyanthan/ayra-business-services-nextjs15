44606258f080aa1f4beab89bcd9e8de6bba2ca29
Samson <samsonjeyanthan5@gmail.com>
2026-02-14 11:14:24 +0530

    SSr issue

--- auth.ts
+++ auth.ts
@@ -4,7 +4,6 @@ import Credentials from "next-auth/providers/credentials";
 import { IUserDoc } from "./database/user.model";
 import { SignInSchema } from "./lib/validations";
 import { api } from "./lib/api";
-import dbConnect from "./lib/mongoose";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -20,8 +19,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           //   email
           // )) as ActionResponse<IAccountDoc>;
           // if (!existingAccount) return null;
-
-          await dbConnect();
 
           const { data: existingUser } = (await api.users.getByEmail(
             email
